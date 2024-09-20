31468f0b85d9ccb28fe26b70534e3f87f403e0da
Abdulloh <rahmonovabdulloh80@gmail.com>
2024-09-20 18:25:34 +0500

    masalalar ozgardi

--- 1-masala.py
+++ 1-masala.py
@@ -1,5 +1,5 @@
-a = int(input("A: ni kiriting: "))
-b = int(input("B: ni kiriting: "))
+a = int(input("A ni kiriting: "))
+b = int(input("B ni kiriting: "))
 
 juft_sonlar = [son for son in range(a,b+1) if son%2==0]
 print(juft_sonlar)
